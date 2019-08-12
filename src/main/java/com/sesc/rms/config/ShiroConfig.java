836214e0a6729f40897aba589fbe4c12501805da
lhf <1490030544@qq.com>
2019-08-12 18:08:43 +0800

    表单过滤器

--- src/main/java/com/sesc/rms/config/ShiroConfig.java
+++ src/main/java/com/sesc/rms/config/ShiroConfig.java
@@ -1,6 +1,7 @@
 package com.sesc.rms.config;
 
 import com.sesc.rms.config.shiro.SescRealm;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +11,17 @@ import org.springframework.context.annotation.Configuration;
 public class ShiroConfig{
 
 
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher(){
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        hashedCredentialsMatcher.setHashIterations(1);
+        return hashedCredentialsMatcher;
+    }
+
     @Bean("sescRealm")
     public SescRealm sescRealm(){
         SescRealm realm = new SescRealm();
+        realm.setCredentialsMatcher(hashedCredentialsMatcher());
         return realm;
     }
     @Bean
@@ -21,4 +30,7 @@ public class ShiroConfig{
         securityManager.setRealm(sescRealm());
         return securityManager;
     }
+
+
+
 }
